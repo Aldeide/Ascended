567eff1c62853575c37ca6564a0094bd6c15f48b
Aldeide <anthony.surleraux@gmail.com>
2025-04-10 18:33:14 +0200

    Start adding basic movement

--- Assets/Systems/Movement/PlayerMovementController.cs
+++ Assets/Systems/Movement/PlayerMovementController.cs
@@ -7,24 +7,65 @@ namespace Systems.Movement
 {
     public class PlayerMovementController : MonoBehaviour
     {
-        private Vector2 movementVector = new Vector2(0, 0);
+        private Vector3 movementInput = new Vector3(0, 0, 0);
+        private Vector2 mousePosition = new Vector2(0, 0);
+        
+        private Animator _animator;
+        private Rigidbody _rigidbody;
+        
         public void Start()
         {
-            
+            _animator = GetComponent<Animator>();
+            _rigidbody = GetComponent<Rigidbody>();
         }
 
         public void Update()
         {
-            this.transform.position += new Vector3(movementVector.x, 0, movementVector.y) * Time.deltaTime; 
+            Vector3 newPos = this.transform.position += Quaternion.AngleAxis(45, Vector3.up) * movementInput * (Time.deltaTime * 3.0f) * 4.0f;
+            _rigidbody.MovePosition(newPos);
+            //Rotate();
+            UpdateAnimator();
+        }
+        
+        private void Rotate()
+        {
+            Ray ray = UnityEngine.Camera.main.ScreenPointToRay(mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Vector3 target = hit.point;
+                target.y = transform.position.y;
+                transform.LookAt(target);
+                // Debug.DrawLine(this.transform.position, hit.point);
+            }
+        }
+
+        private void UpdateAnimator()
+        {
+            if (movementInput.magnitude > 0.01f)
+            {
+                _animator.SetBool("IsMoving", true);
+                _animator.SetFloat("MovementX", movementInput.x);
+                _animator.SetFloat("MovementY", movementInput.y);
+            }
+            else
+            {
+                _animator.SetBool("IsMoving", false);
+            }
         }
 
         public void OnMoveAction(InputAction.CallbackContext context)
         {
             if (context.phase == InputActionPhase.Performed || context.phase == InputActionPhase.Canceled)
             {
-                Debug.Log(context.ReadValue<Vector2>());
-                movementVector = context.ReadValue<Vector2>();
+                Vector2 input = context.ReadValue<Vector2>();
+                movementInput = new Vector3(input.x, 0, input.y);
             }
+        }
+        
+        
+        public void OnLookInput(InputAction.CallbackContext context)
+        {
+            mousePosition = context.ReadValue<Vector2>();
         }
     }
 }
