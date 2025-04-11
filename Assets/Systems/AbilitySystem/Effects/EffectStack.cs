76957756509d11cf842241bcb6183688e78c6198
Aldeide <anthony.surleraux@gmail.com>
2025-04-11 17:52:42 +0200

    Add Effect stack class to define effect stacking behaviour

---
+++ Assets/Systems/AbilitySystem/Effects/EffectStack.cs
@@ -0,0 +1,45 @@
+﻿namespace Systems.AbilitySystem.Effects
+{
+    public enum EffectStackType
+    {
+        None,
+        AggregateBySource,
+        AggregateByTarget
+    }
+
+    public enum EffectStackRefreshPolicy
+    {
+        NeverRefresh,
+        RefreshOnNewApplication
+    }
+
+    public enum EffectStackDurationPolicy
+    {
+        NeverRefresh,
+        RefreshOnNewApplication
+    }
+
+    public enum EffectStackExpirationPolicy
+    {
+        ClearAllStacks,
+        RemoveSingleStackAndRefreshDuration
+    }
+
+    public struct EffectStackOverflowPolicy
+    {
+        public bool ClearStackOnOverflow;
+        public bool DenyOverflowApplication;
+        public Effect[] OverflowEffects;
+    }
+    
+    public struct EffectStack
+    {
+        public EffectStackType EffectStackType;
+        public int MaxStacks;
+        public EffectStackDurationPolicy EffectStackDurationPolicy;
+        public EffectStackRefreshPolicy EffectStackRefreshPolicy;
+        public EffectStackExpirationPolicy EffectStackExpirationPolicy;
+        public EffectStackOverflowPolicy EffectStackOverflowPolicy;
+
+    }
+}
