3464ad8a634589b7548c9a0c8233622787ff616b
Aldeide <anthony.surleraux@gmail.com>
2025-04-18 20:22:29 +0200

    Integrate movement speed attribute in movement controller

--- Assets/Systems/Movement/PlayerMovementController.cs
+++ Assets/Systems/Movement/PlayerMovementController.cs
@@ -1,4 +1,8 @@
 ﻿
+using AbilitySystem.Runtime.Attributes;
+using AbilitySystem.Runtime.Core;
+using AbilitySystem.Scripts;
+using AbilitySystemExtension.Runtime.AttributeSets;
 using NUnit.Framework;
 using Unity.Mathematics;
 using Unity.Netcode;
@@ -10,16 +14,19 @@ namespace Systems.Movement
 {
     public class PlayerMovementController : NetworkBehaviour
     {
-        private Vector3 movementInput = new Vector3(0, 0, 0);
+        private Vector3 _movementInput = new Vector3(0, 0, 0);
 
         [SerializeField] private GameObject cameraTarget;
         
         private Animator _animator;
         private Rigidbody _rigidbody;
+        private IAbilitySystem _abilitySystem;
+        private float _movementSpeed;
         
         public float turnSmoothTime = 0.1f;   
         private float turnSmoothVelocity = 2.0f;
-
+        
+        
         public override void OnNetworkSpawn()
         {
             
@@ -29,28 +36,28 @@ namespace Systems.Movement
         {
             _animator = GetComponent<Animator>();
             _rigidbody = GetComponent<Rigidbody>();
+            _abilitySystem = GetComponent<AbilitySystemComponent>().AbilitySystem;
+            _abilitySystem.AttributeSetManager.RegisterOnAttributeChanged("MovementSpeed", OnMovementSpeedChanged);
+            _movementSpeed = _abilitySystem.AttributeSetManager.GetAttributeSet<CharacteristicsAttributeSet>()
+                .MovementSpeed.CurrentValue;
         }
 
         public void Update()
         {
-            Debug.Log(IsOwner);
             if (!IsLocalPlayer) return;
             
-            if (movementInput.magnitude <= 0.01f)
+            if (_movementInput.magnitude <= 0.01f)
             {
                 UpdateAnimator();
                 return;
             }
             
-            float targetAngle = Mathf.Atan2(movementInput.x, movementInput.z) * Mathf.Rad2Deg +
+            float targetAngle = Mathf.Atan2(_movementInput.x, _movementInput.z) * Mathf.Rad2Deg +
                                 UnityEngine.Camera.main.transform.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            //cameraTarget.transform.Rotate(new Vector3(0f, -angle, 0f));
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-
-            _rigidbody.MovePosition(this.transform.position += moveDirection * Time.deltaTime * 2.0f);
-            //Rotate(this.transform.position + movement);
+            _rigidbody.MovePosition(this.transform.position += moveDirection * Time.deltaTime * _movementSpeed);
             UpdateAnimator();
         }
         
@@ -59,12 +66,11 @@ namespace Systems.Movement
             Vector3 lookAtTarget = new Vector3(newPosition.x, this.transform.position.y, newPosition.z);
             Quaternion rotation = quaternion.LookRotation(lookAtTarget, Vector3.up);
             transform.LookAt(lookAtTarget);
-            //cameraTarget.transform.rotation *= Quaternion.Inverse(rotation);
         }
 
         private void UpdateAnimator()
         {
-            if (movementInput.magnitude > 0.01f)
+            if (_movementInput.magnitude > 0.01f)
             {
                 _animator.SetBool("IsMoving", true);
                 _animator.SetFloat("MovementX", 0.0f);
@@ -81,13 +87,18 @@ namespace Systems.Movement
             if (context.phase == InputActionPhase.Performed || context.phase == InputActionPhase.Canceled)
             {
                 Vector2 input = context.ReadValue<Vector2>();
-                movementInput = new Vector3(input.x, 0, input.y);
+                _movementInput = new Vector3(input.x, 0, input.y);
             }
         }
         
         public void OnLookInput(InputAction.CallbackContext context)
         {
             //mousePosition = context.ReadValue<Vector2>();
+        }
+
+        public void OnMovementSpeedChanged(Attribute attribute, float oldValue, float newValue)
+        {
+            _movementSpeed = newValue;
         }
     }
 }
