9b5f5ba42dcec77aa4cfa126eac30dfd62f769ff
Aldeide <anthony.surleraux@gmail.com>
2025-04-24 20:51:33 +0200

    Add some comments

--- Assets/AbilitySystem/Runtime/Modifiers/FloatModifier.cs
+++ Assets/AbilitySystem/Runtime/Modifiers/FloatModifier.cs
@@ -7,6 +7,7 @@ namespace AbilitySystem.Runtime.Modifiers
     [Serializable]
     public class FloatModifier : Modifier
     {
+        // A simple modifier that adds, subtracts, multiplies or divides a modifier with a single float value.
         [SerializeField]
         public float ModifierMagnitude = 1;
         
--- Assets/AbilitySystem/Runtime/Modifiers/Modifier.cs
+++ Assets/AbilitySystem/Runtime/Modifiers/Modifier.cs
@@ -4,6 +4,12 @@ using Sirenix.OdinInspector;
 
 namespace AbilitySystem.Runtime.Modifiers
 {
+    // This abstract class defines the notion of a modifier. Modifiers are applied by Effects and change the value of
+    // an attribute. Instant effects change the base value of an attribute and durational or permanent effects change
+    // the current value of an attribute.
+    // As such, the current value of an attribute is (base + additive_modifiers) * multiplicative_modifiers.
+    // Additionally, some modifiers can be of an 'override' type. In that case, the last override modifier to be applied
+    // is the one selected.
     [Serializable]
     public abstract class Modifier
     {
