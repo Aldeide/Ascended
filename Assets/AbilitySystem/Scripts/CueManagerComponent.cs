044962e3a4759b49187c56055b7aa874a452ea65
Aldeide <anthony.surleraux@gmail.com>
2025-04-24 21:04:20 +0200

    Add a Cue Manager

---
+++ Assets/AbilitySystem/Scripts/CueManagerComponent.cs
@@ -0,0 +1,18 @@
+﻿using AbilitySystem.Runtime.Core;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace AbilitySystem.Scripts
+{
+    // Component that handles the execution of cues to run cosmetics (vfx, audio, animation, ...).
+    [RequireComponent(typeof(AbilitySystemComponent))]
+    public class CueManagerComponent : NetworkBehaviour
+    {
+        private IAbilitySystem _abilitySystem;
+        public void Start()
+        {
+            _abilitySystem = GetComponent<AbilitySystemComponent>().AbilitySystem;
+            // TODO: subscribe to tags being added or removed.
+        }
+    }
+}
