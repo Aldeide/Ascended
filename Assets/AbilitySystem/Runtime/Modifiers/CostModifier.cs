f25f2f42980d48aa0590cb6224adb0a27d706873
Aldeide <anthony.surleraux@gmail.com>
2025-04-28 16:49:59 +0200

    Add a CostModifier

---
+++ Assets/AbilitySystem/Runtime/Modifiers/CostModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AbilitySystem.Runtime.Effects;
+using Sirenix.OdinInspector;
+
+namespace AbilitySystem.Runtime.Modifiers
+{
+    [Serializable]
+    public class CostModifier : Modifier
+    {
+        [ValueDropdown("@DropdownValuesUtil.AttributeChoices", IsUniqueList = true)]
+        public string costMetaAttribute;
+
+        public float baseCost;
+        public override float Calculate(Effect effect)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
