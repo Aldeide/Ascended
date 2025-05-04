c4d22e89693b4d52aaf25778eb2f1d7c36443638
Aldeide <anthony.surleraux@gmail.com>
2025-05-04 19:11:04 +0200

    Add a unit test for AbilityDefinition

---
+++ Assets/AbilitySystem/Test/Runtime/Abilities/AbilityDefinitionTests.cs
@@ -0,0 +1,21 @@
+﻿using AbilitySystem.Runtime.Abilities.PassiveAbility;
+using NUnit.Framework;
+using static AbilitySystem.Test.Utilities.AbilityUtilities;
+
+namespace AbilitySystem.Test.Runtime.Abilities
+{
+    public class AbilityDefinitionTests
+    {
+        [Test]
+        public void AbilityDefinitionTests_CreationDefault_HasCorrectValues()
+        {
+            var abilityDefinition = CreatePassiveAbilityDefinition();
+
+            Assert.AreEqual("TestAbility", abilityDefinition.uniqueName);
+            Assert.AreEqual("PassiveAbilityDefinition", abilityDefinition.TypeName);
+            Assert.AreEqual(typeof(PassiveAbility), abilityDefinition.AbilityType());
+            Assert.AreEqual("AbilitySystem.Runtime.Abilities.PassiveAbility.PassiveAbilityDefinition",
+                abilityDefinition.TypeFullName);
+        }
+    }
+}
