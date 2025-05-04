179ca0f0c99f14beeb847bc103f71b813a34fb6f
Aldeide <anthony.surleraux@gmail.com>
2025-05-04 19:11:04 +0200

    Add a unit test for AbilityDefinition

---
+++ Assets/AbilitySystem/Test/Utilities/AbilityUtilities.cs
@@ -0,0 +1,18 @@
+﻿using AbilitySystem.Runtime.Abilities;
+using AbilitySystem.Runtime.Abilities.PassiveAbility;
+using AbilitySystem.Runtime.Tags;
+using UnityEngine;
+
+namespace AbilitySystem.Test.Utilities
+{
+    public static class AbilityUtilities
+    {
+        public static PassiveAbilityDefinition CreatePassiveAbilityDefinition()
+        {
+            PassiveAbilityDefinition abilityDefinition = ScriptableObject.CreateInstance<PassiveAbilityDefinition>();
+            abilityDefinition.AbilityTags = new AbilityTags();
+            abilityDefinition.uniqueName = "TestAbility";
+            return abilityDefinition;
+        }
+    }
+}
