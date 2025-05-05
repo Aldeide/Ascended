1a3405a2228a0e9aaadc4f126eb535540ada155a
Aldeide <anthony.surleraux@gmail.com>
2025-05-05 03:59:33 +0200

    Add basic ability cooldown

---
+++ Assets/AbilitySystem/Runtime/Abilities/Cooldowns/AbilityCooldown.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AbilitySystem.Runtime.Abilities.Cooldowns
+{
+    [Serializable]
+    public abstract class AbilityCooldown
+    {
+        public float BaseDuration;
+
+        protected float _currentCooldown;
+        
+        public abstract float Calculate();
+
+        public abstract float RemainingCooldown();
+
+        public abstract void Tick();
+    }
+}
---
+++ Assets/AbilitySystem/Runtime/Abilities/Cooldowns/ConstantAbilityCooldown.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace AbilitySystem.Runtime.Abilities.Cooldowns
+{
+    [Serializable]
+    public class ConstantAbilityCooldown : AbilityCooldown
+    {
+        public override float Calculate()
+        {
+            return BaseDuration;
+        }
+
+        public override float RemainingCooldown()
+        {
+            return _currentCooldown;
+        }
+
+        public override void Tick()
+        {
+            if (_currentCooldown == 0) return;
+            _currentCooldown -= Time.deltaTime;
+            if (_currentCooldown < 0) _currentCooldown = 0;
+        }
+    }
+}
