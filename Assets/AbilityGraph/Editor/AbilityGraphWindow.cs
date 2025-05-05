58722dde908458311539ed481e06f367831798c6
Aldeide <anthony.surleraux@gmail.com>
2025-05-05 19:38:52 +0200

    blabla

--- Assets/AbilityGraph/Editor/AbilityGraphWindow.cs
+++ Assets/AbilityGraph/Editor/AbilityGraphWindow.cs
@@ -1,9 +1,30 @@
-﻿using UnityEditor;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
 
-namespace AbilityGraph.Editor
+public class AbilityGraphWindow : EditorWindow
 {
-    public class AbilityGraphWindow : EditorWindow
+    [SerializeField]
+    private VisualTreeAsset m_VisualTreeAsset = default;
+
+    [MenuItem("Window/UI Toolkit/AbilityGraphWindow")]
+    public static void ShowExample()
     {
-        
+        AbilityGraphWindow wnd = GetWindow<AbilityGraphWindow>();
+        wnd.titleContent = new GUIContent("AbilityGraphWindow");
+    }
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+
+        // VisualElements objects can contain other VisualElement following a tree hierarchy.
+        VisualElement label = new Label("Hello World! From C#");
+        root.Add(label);
+
+        // Instantiate UXML
+        VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
+        root.Add(labelFromUXML);
     }
 }
---
+++ Assets/AbilityGraph/Editor/AbilityGraphWindowInitialiser.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+namespace AbilityGraph.Editor
+{
+    public class AbilityGraphWindowInitialiser
+    {
+        [MenuItem("Ascendant/Ability")]
+        static void OpenWindow() {
+            // Here you can define what type of graph model you would like to use.
+            //AbilityGraphWindow
+            //GraphWindow.InitializeWindowBase(typeof(ScriptableGraphModel));
+        }
+    }
+}
