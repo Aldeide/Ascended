0d452afd891e5c2e7204fec00c3c93d43534daac
Aldeide <anthony.surleraux@gmail.com>
2025-05-05 04:17:45 +0200

    Add gameplay events

---
+++ Assets/AbilitySystem/Runtime/Events/GameplayEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AbilitySystem.Runtime.Events
+{
+    public abstract class GameplayEvent<T>
+    {
+        public Type Type { get; private set; }
+        public T Arguments { get; private set; }
+
+        protected GameplayEvent(Type type, T arguments)
+        {
+            Type = type;
+            Arguments = arguments;
+        }
+    }
+}
