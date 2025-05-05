dd50ff971cd55d6174e90e8301db05ecc1c13d56
Aldeide <anthony.surleraux@gmail.com>
2025-05-05 23:05:13 +0200

    Add a durational ability type

--- Assets/AbilitySystem/Runtime/AbilityTasks/DurationalAbilityTask.cs
+++ Assets/AbilitySystem/Runtime/AbilityTasks/DurationalAbilityTask.cs
@@ -1,18 +1,28 @@
-﻿using AbilitySystem.Runtime.Abilities;
+﻿using System;
+using AbilitySystem.Runtime.Abilities;
 
 namespace AbilitySystem.Runtime.AbilityTasks
 {
+    [Serializable]
     public abstract class DurationalAbilityTask : AbilityTask
     {
         public float StartTime;
         public float Duration;
-        public DurationalAbilityTask(Ability owningAbility) : base(owningAbility)
-        {
-        }
-
+        
         public abstract void Start();
         public abstract void Tick();
         public abstract void End();
 
+        public void Reset()
+        {
+            StartTime = -1;
+        }
+
+        public bool HasEnded()
+        {
+            return StartTime + Duration <= _owner.GetTime();
+        }
+        
+        public bool IsActive => StartTime > 0;
     }
 }
