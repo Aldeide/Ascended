dd02bbb339696bb2c3613e5d317f8d1fda977c28
Aldeide <anthony.surleraux@gmail.com>
2025-05-08 23:28:19 +0200

    Add PlayAudioCue node

---
+++ Assets/AbilityGraph/Runtime/Nodes/Cues/PlayAudioCue.cs
@@ -0,0 +1,19 @@
+﻿using AbilityGraph.Runtime.Nodes.Base;
+using AbilitySystem.Runtime.Cues;
+using GraphProcessor;
+
+namespace AbilityGraph.Runtime.Nodes.Cues
+{
+    [System.Serializable, NodeMenuItem("Cues/PlayAudioCue")]
+    public class PlayAudioCue : LinearExecutableNode
+    {
+        public override string name => "Play Audio Cue";
+        
+        public CueAudioDefinition CueDefinition;
+        
+        protected override void Process()
+        {
+            Owner.PlayCue(CueDefinition);
+        }
+    }
+}
