b9efacc1b1b2eaf677f72851590ff2a43d40641e
Aldeide <anthony.surleraux@gmail.com>
2025-05-13 20:34:21 +0200

    Add a GameplayTagQuery struct and some tests

---
+++ Assets/AbilitySystem/Runtime/Tags/GameplayTagQuery.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace AbilitySystem.Runtime.Tags
+{
+    [Serializable]
+    public struct GameplayTagQuery
+    {
+        public GameplayTagCondition[] Condition;
+
+        public GameplayTagQuery(params GameplayTagCondition[] condition)
+        {
+            Condition = condition;
+        }
+        
+        public bool MatchesTag(GameplayTag tag)
+        {
+            return Condition.All(condition => condition.MatchesTag(tag));
+        }
+    }
+
+    [Serializable]
+    public struct GameplayTagCondition
+    {
+        public GameplayTagMatchType MatchType;
+        public GameplayTag[] Tags;
+
+        public GameplayTagCondition(GameplayTagMatchType matchType, params GameplayTag[] tags)
+        {
+            MatchType = matchType;
+            Tags = tags;
+        }
+        
+        public bool MatchesTag(GameplayTag tag)
+        {
+            if (MatchType == GameplayTagMatchType.AnyOfExact && Tags.Any(t => t.Equals(tag))) return true;
+            if (MatchType == GameplayTagMatchType.AllOfExact && Tags.All(t => t.Equals(tag))) return true;
+            if (MatchType == GameplayTagMatchType.NoneOfExact && !Tags.Any(t => t.Equals(tag))) return true;
+            if (MatchType == GameplayTagMatchType.AnyOfPartial && Tags.Any(t => t.HasTag(tag))) return true;
+            if (MatchType == GameplayTagMatchType.AllOfPartial && Tags.All(t => t.HasTag(tag))) return true;
+            if (MatchType == GameplayTagMatchType.NoneOfPartial && !Tags.Any(t => t.HasTag(tag))) return true;
+            return false;
+        }
+    }
+
+    [Serializable]
+    public enum GameplayTagMatchType
+    {
+        AnyOfExact,
+        AllOfExact,
+        NoneOfExact,
+        AnyOfPartial,
+        AllOfPartial,
+        NoneOfPartial,
+    }
+}
---
+++ Assets/AbilitySystem/Test/Runtime/Tags/GameplayTagQueryTests.cs
@@ -0,0 +1,55 @@
+﻿using AbilitySystem.Runtime.Tags;
+using NUnit.Framework;
+
+namespace AbilitySystem.Test.Runtime.Tags
+{
+    public class GameplayTagQueryTests
+    {
+        [Test]
+        public void GameplayTagQueryTests_AnyOfExactNonMatching_ReturnsFalse()
+        {
+            var testTagA = new GameplayTag("Test.Tag.A");
+            var testTagB = new GameplayTag("Test.Tag.B");
+            var testTagC = new GameplayTag("Test.Tag.C");
+            var conditionOne = new GameplayTagCondition(GameplayTagMatchType.AnyOfExact, testTagA, testTagB);
+            var tagQuery = new GameplayTagQuery(conditionOne);
+            
+            Assert.IsFalse(tagQuery.MatchesTag(testTagC));
+        }
+        
+        [Test]
+        public void GameplayTagQueryTests_AnyOfExactMatching_ReturnsTrue()
+        {
+            var testTagA = new GameplayTag("Test.Tag.A");
+            var testTagB = new GameplayTag("Test.Tag.B");
+            var conditionOne = new GameplayTagCondition(GameplayTagMatchType.AnyOfExact, testTagA, testTagB);
+            var tagQuery = new GameplayTagQuery(conditionOne);
+            
+            Assert.IsTrue(tagQuery.MatchesTag(testTagB));
+        }
+        
+        [Test]
+        public void GameplayTagQueryTests_AnyOfPartialNonMatching_ReturnsFalse()
+        {
+            var testTagA = new GameplayTag("Test.Tag.A");
+            var testTagB = new GameplayTag("Test.Tag.B");
+            var testTagC = new GameplayTag("Test.Tag.C");
+            var conditionOne = new GameplayTagCondition(GameplayTagMatchType.AnyOfPartial, testTagA, testTagB);
+            var tagQuery = new GameplayTagQuery(conditionOne);
+            
+            Assert.IsFalse(tagQuery.MatchesTag(testTagC));
+        }
+        
+        [Test]
+        public void GameplayTagQueryTests_AnyOfPartialMatching_ReturnsTrue()
+        {
+            var testTagA = new GameplayTag("Test.Tag.A");
+            var testTagB = new GameplayTag("Test.Tag.B");
+            var testTagC = new GameplayTag("Test.Tag");
+            var conditionOne = new GameplayTagCondition(GameplayTagMatchType.AnyOfPartial, testTagA, testTagB);
+            var tagQuery = new GameplayTagQuery(conditionOne);
+            
+            Assert.IsTrue(tagQuery.MatchesTag(testTagC));
+        }
+    }
+}
