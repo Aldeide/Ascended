578411c506262b889154eacb56664468eb08f07c
Aldeide <anthony.surleraux@gmail.com>
2025-05-13 20:34:21 +0200

    Add a GameplayTagQuery struct and some tests

--- Assets/AbilitySystem/Runtime/Cues/CueData.cs
+++ Assets/AbilitySystem/Runtime/Cues/CueData.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using AbilitySystem.Runtime.Networking;
+﻿using AbilitySystem.Runtime.Networking;
 using Unity.Netcode;
 using UnityEngine;
 
 namespace AbilitySystem.Runtime.Cues
 {
+    /// Represents data used for handling gameplay cues, including manipulation and transmission over the network.
     public class CueData : INetworkSerializable, IData
     {
         public Vector3[] VectorData;
