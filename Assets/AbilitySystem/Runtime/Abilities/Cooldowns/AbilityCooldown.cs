98e93e1fb1caa830cfdb892e166b79fe04ee175b
Aldeide <anthony.surleraux@gmail.com>
2025-05-13 23:25:06 +0200

    Add some unit tests for ability cooldowns

--- Assets/AbilitySystem/Runtime/Abilities/Cooldowns/AbilityCooldown.cs
+++ Assets/AbilitySystem/Runtime/Abilities/Cooldowns/AbilityCooldown.cs
@@ -18,29 +18,22 @@ namespace AbilitySystem.Runtime.Abilities.Cooldowns
     [Serializable]
     public abstract class AbilityCooldown
     {
-        public float BaseDuration;
-
         [ValueDropdown("@DropdownValuesUtil.GameplayTagChoices", IsUniqueList = true, HideChildProperties = true)]
         public GameplayTag CooldownTag;
-
+        public EffectDefinition CooldownEffect;
+        
         public abstract float Calculate(IAbilitySystem owner);
 
         public virtual bool CanActivate(IAbilitySystem owner)
         {
-            return owner.EffectManager.GetEffect(CooldownTag) == null;
+            return !owner.TagManager.HasAnyTags(CooldownEffect.grantedTags);
         }
 
         public virtual bool Activate(IAbilitySystem owner)
         {
             if (!CanActivate(owner)) return false;
-
-            // TODO: have the effect be predefined for easier replication. (or define a version of the Effect class that
-            // can be nicely replicated).
-            var effectDefinition = ScriptableObject.CreateInstance<EffectDefinition>();
-            effectDefinition.assetTags = new[] { CooldownTag };
-            effectDefinition.durationType = EffectDurationType.FixedDuration;
-            effectDefinition.durationSeconds = Calculate(owner);
-            var effect = effectDefinition.ToEffect(owner, owner);
+            
+            var effect = CooldownEffect.ToEffect(owner, owner);
             effect.Activate();
             owner.EffectManager.AddEffect(effect);
             return true;
