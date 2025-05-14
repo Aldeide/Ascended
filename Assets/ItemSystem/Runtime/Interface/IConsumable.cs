bd96f89ff3a2b4fa0d102ff19cb6db8602b0533c
Elgorag <bengervet@gmail.com>
2025-05-14 14:41:25 +0200

    Add the basis for ItemSystem

---
+++ Assets/ItemSystem/Runtime/Interface/IConsumable.cs
@@ -0,0 +1,16 @@
+﻿using AbilitySystem.Runtime.Abilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.ItemSystem.Runtime.Interface
+{
+    public interface IConsumable : IBaseItem
+    {
+        protected Ability ConsumableAbility { get; }
+
+        Ability Consume();
+    }
+}
