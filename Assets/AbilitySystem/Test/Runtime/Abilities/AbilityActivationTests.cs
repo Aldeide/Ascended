2980cfad2699098c9a37d9bc396a6b9c6ccd3ab7
Aldeide <anthony.surleraux@gmail.com>
2025-05-22 19:49:22 +0200

    Add unit test for event-based ability activation
    Introduced `AbilityActivationTests` to verify that abilities activate correctly upon receiving specific events. Updated `Ability.OnActivationEvent` to call `TryActivateAbility` for proper activation handling.

---
+++ Assets/AbilitySystem/Test/Runtime/Abilities/AbilityActivationTests.cs
@@ -0,0 +1,31 @@
+﻿using AbilitySystem.Runtime.Abilities.AbilityActivation;
+using AbilitySystem.Test.Utilities;
+using NUnit.Framework;
+
+using static AbilitySystem.Test.Utilities.AbilityUtilities;
+using static AbilitySystem.Test.Utilities.AbilitySystemUtilities;
+
+namespace AbilitySystem.Test.Runtime.Abilities
+{
+    public class AbilityActivationTests
+    {
+        [Test]
+        public void AbilityActivationTests_EventActivation_ActivatesOnEvent()
+        {
+            var abilitySystem = CreateMockServerAbilitySystem().Object;
+            var abilityDefinition = CreateInstantAbilityDefinition();
+            var eventActivation = new OnEventActivation
+            {
+                ActivationEvent = new TestGameplayEventType()
+            };
+            abilityDefinition.AbilityActivation = eventActivation;
+            abilitySystem.AbilityManager.GrantAbility(abilityDefinition);
+            var eventManager = abilitySystem.EventManager;
+            var eventArgs = new TestGameplayEventArgs();
+            
+            eventManager.TriggerEvent(new TestGameplayEvent(eventArgs));
+            
+            Assert.IsTrue(abilitySystem.AbilityManager.Abilities[abilityDefinition.uniqueName].IsActive);
+        }
+    }
+}
