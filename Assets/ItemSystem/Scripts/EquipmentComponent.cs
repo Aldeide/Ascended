e21eb453106370d64bf9298973ab6b34d39326cf
Aldeide <anthony.surleraux@gmail.com>
2025-05-31 15:00:27 +0200

    Add Equipment and Inventory Components

---
+++ Assets/ItemSystem/Scripts/EquipmentComponent.cs
@@ -0,0 +1,24 @@
+﻿using AbilitySystem.Scripts;
+using ItemSystem.Runtime.Manager;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace ItemSystem.Scripts
+{
+    [RequireComponent(typeof(AbilitySystemComponent))]
+    public class EquipmentComponent : NetworkBehaviour
+    {
+        public EquipmentManager EquipmentManager;
+        
+        private AbilitySystemComponent _abilitySystemComponent;
+        private InventoryComponent _inventoryComponent;
+        
+        public override void OnNetworkSpawn()
+        {
+            base.OnNetworkSpawn();
+            _abilitySystemComponent = GetComponent<AbilitySystemComponent>();
+            _inventoryComponent = GetComponent<InventoryComponent>();
+            EquipmentManager = new EquipmentManager(_abilitySystemComponent.AbilitySystem);
+        }
+    }
+}
