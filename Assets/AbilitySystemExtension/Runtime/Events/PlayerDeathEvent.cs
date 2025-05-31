73414da7a1e542bb746ecf1f0eeb29babc229b6e
Aldeide <anthony.surleraux@gmail.com>
2025-05-31 15:18:02 +0200

    Add PlayerDeathEvent

---
+++ Assets/AbilitySystemExtension/Runtime/Events/PlayerDeathEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AbilitySystem.Runtime.Events;
+
+namespace AbilitySystemExtension.Runtime.Events
+{
+    [Serializable]
+    public class PlayerDeathEvent : GameplayEvent
+    {
+        public PlayerDeathEvent(PlayerDeathEventArgs arguments) : base(arguments)
+        {
+        }
+    }
+
+    [Serializable]
+    public class PlayerDeathEventArgs : EventArgs
+    {
+        public float Amount { get; private set;}
+        
+        public PlayerDeathEventArgs(float amount)
+        {
+            Amount = amount;
+        }
+    }
+
+    [Serializable]
+    public class PlayerDeathEventType : GameplayEventType
+    {
+        public override Type EventType => typeof(PlayerDeathEvent);
+    }
+}
