90e0f54f44061ccd99b090e9c2bd0d5fbaa5cd6e
Aldeide <anthony.surleraux@gmail.com>
2025-06-02 21:08:55 +0200

    Only the local player can change the target location.

--- Assets/Systems/Controllers/PlayerTargetController.cs
+++ Assets/Systems/Controllers/PlayerTargetController.cs
@@ -1,47 +1,51 @@
-﻿using UnityEngine;
+﻿using Unity.Netcode;
+using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 namespace Systems.Controllers
 {
-    public class PlayerTargetController : MonoBehaviour
+    public class PlayerTargetController : NetworkBehaviour
     {
-        public GameObject target;
-        public LayerMask layerMask;
+        [FormerlySerializedAs("target")] public GameObject Target;
+        [FormerlySerializedAs("layerMask")] public LayerMask LayerMask;
+        
+        private UnityEngine.Camera _camera;
+        
         public void Start()
         {
-            
+            _camera = UnityEngine.Camera.main;
         }
 
         public Vector3 GetTargetPosition()
         {
-            return target.transform.position;
+            return Target.transform.position;
         }
 
         public void OnLook(InputAction.CallbackContext context)
         {
+            if (!IsLocalPlayer) return;
             if (context.phase != InputActionPhase.Performed) return;
             UpdateTarget();
         }
 
-        public void UpdateTarget()
+        private void UpdateTarget()
         {
-            Vector3 rayOrigin = new Vector3(0.5f, 0.5f, 0f); // center of the screen
-            float rayLength = 500f;
-            Ray ray = UnityEngine.Camera.main.ViewportPointToRay(rayOrigin);
+            var rayOrigin = new Vector3(0.5f, 0.5f, 0f); // center of the screen
+            const float rayLength = 500f;
+            var ray = _camera.ViewportPointToRay(rayOrigin);
+            
             Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.red);
-
-            RaycastHit hit;
             
-            
-            if (Physics.Raycast(ray, out hit, rayLength, layerMask, QueryTriggerInteraction.Ignore))
+            if (Physics.Raycast(ray, out var hit, rayLength, LayerMask, QueryTriggerInteraction.Ignore))
             {
-                target.transform.position = hit.point;
+                Target.transform.position = hit.point;
             }
         }
 
         private void OnDrawGizmos()
         {
-            Gizmos.DrawCube(target.transform.position, new Vector3(0.1f, 0.1f, 0.1f));
+            Gizmos.DrawCube(Target.transform.position, new Vector3(0.1f, 0.1f, 0.1f));
         }
     }
 }
