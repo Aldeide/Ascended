abd45dbe9fa6eb7a9eb1d8fce986d426207ad531
Elgorag <bengervet@gmail.com>
2025-06-02 20:45:58 +0200

    ItemSystem in progress

---
+++ Assets/ItemSystem/Runtime/Definition/EquipableDefinition.cs
@@ -0,0 +1,30 @@
+﻿using AbilitySystem.Runtime.Abilities;
+using Assets.ItemSystem.Runtime.Constants;
+using Assets.ItemSystem.Runtime.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.ItemSystem.Runtime.Definition
+{
+    public class EquipableDefinition : ItemDefinition, IEquippable
+    {
+        public List<EquipableSlot> Mods => throw new NotImplementedException();
+
+        public Dictionary<ModType, int> MaxModByType => throw new NotImplementedException();
+
+        public Ability EquippableAbility => throw new NotImplementedException();
+
+        public void EquipItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnequipItem()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/ItemSystem/Runtime/Definition/ItemDefinition.cs
@@ -0,0 +1,16 @@
+﻿using Assets.ItemSystem.Runtime.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.ItemSystem.Runtime.Definition
+{
+    [Serializable]
+    public class ItemDefinition : ScriptableObject, IBaseItem
+    {
+        public string Name => throw new NotImplementedException();
+    }
+}
