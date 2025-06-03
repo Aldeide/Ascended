846e86e60a3065e91cbcea3b5a22c72828f9ef1f
Aldeide <anthony.surleraux@gmail.com>
2025-06-03 20:56:02 +0200

    Add unit tests for AbilityGraph primitive nodes
    Introduced `PrimitivesNodesTests` to test `FloatNode` and `IntNode` functionality in the AbilityGraph system. Verified that output values match input values after processing. Also added necessary assembly definition files for testing.

---
+++ Assets/AbilityGraph/Tests/Runtime/Nodes/Primitives/PrimitivesNodesTests.cs
@@ -0,0 +1,30 @@
+﻿using AbilityGraph.Runtime.Nodes.Primitives;
+using NUnit.Framework;
+
+namespace AbilityGraph.Tests.Runtime.Nodes.Primitives
+{
+    public class PrimitivesNodesTests
+    {
+        [Test]
+        public void PrimitiveNodeTest_FloatNode_OutputIsEqualToInput()
+        {
+            var node = new FloatNode
+            {
+                input = 10
+            };
+            node.OnProcess();
+            Assert.AreEqual(node.input, node.output);
+        }
+        
+        [Test]
+        public void PrimitiveNodeTest_IntNode_OutputIsEqualToInput()
+        {
+            var node = new IntNode
+            {
+                Input = 10
+            };
+            node.OnProcess();
+            Assert.AreEqual(node.Input, node.Output);
+        }
+    }
+}
