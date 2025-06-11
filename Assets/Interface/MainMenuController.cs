3f18e150240cbcbe1e37a050be38b83c1ab5b8b3
Aldeide <anthony.surleraux@gmail.com>
2025-06-11 20:36:33 +0200

    Add Settings menu integration to MainMenu
    Implemented `SettingsMenu` in the Main Menu with a new `MainMenuController` script for navigation logic. Added `SettingsMenuDocument` UXML file and updated `MainMenuEditor` to reference the new settings template, enabling seamless transitions between main menu and settings views.

---
+++ Assets/Interface/MainMenuController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Interface
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        public UIDocument SettingsMenu;
+        public UIDocument _document;
+        private Button _playButton;
+        private Button _settingsButton;
+
+        private Label _mainMenu;
+        private TemplateContainer _settingsTemplate;
+        private void Start()
+        {
+            _document = GetComponent<UIDocument>();
+            SettingsMenu.rootVisualElement.visible = false;
+            _playButton = _document.rootVisualElement.Q<Button>("PlayButton");
+            _settingsButton = _document.rootVisualElement.Q<Button>("SettingsButton");
+            
+            _playButton.clicked += OnPlayButtonClicked;
+            _settingsButton.clicked += OnSettingsButtonClicked;
+
+            _mainMenu = _document.rootVisualElement.Q<Label>("MainMenu");
+            _settingsTemplate = _document.rootVisualElement.Q<TemplateContainer>("SettingsTemplate");
+            _settingsTemplate.visible = false;
+            _mainMenu.visible = true;
+        }
+
+        private static void OnPlayButtonClicked()
+        {
+            Debug.Log("PlayButtonClicked");
+        }
+        
+        private void OnSettingsButtonClicked()
+        {
+            _mainMenu.visible = false;
+            _settingsTemplate.visible = true;
+            Debug.Log("SettingsButtonClicked");
+            //_document.rootVisualElement.visible = false;
+        }
+    }
+}
