da21c8623990a2413c77809c77f4d856bf37520b
Aldeide <anthony.surleraux@gmail.com>
2025-06-16 22:00:22 +0200

    Add GameLoading and MainMenu scenes and initialize Steam
    This commit includes the addition of GameLoadingScene and MainMenu to EditorBuildSettings, alongside the setup of a loading UI. Updated `SteamController.cs` to initialize SteamWorks with a constant AppID and load the MainMenu scene directly. Changes also feature updates to the main camera's settings and HDR/anti-aliasing tweaks.

---
+++ Assets/Gameplay/Scripts/LoadingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Steamworks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Gameplay.Scripts
+{
+    public class LoadingController : MonoBehaviour
+    {
+        SteamController steamController;
+
+        private void Start()
+        {
+            //steamController = FindAnyObjectByType<SteamController>();
+            
+        }
+        
+    }
+}
