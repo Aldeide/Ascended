ef4c502efa0f6206a53fb1c7c24d37374049afcd
Aldeide <anthony.surleraux@gmail.com>
2025-06-16 22:17:41 +0200

    Add AbilityStartEvent to extend gameplay event system
    Introduced a new event, AbilityStartEvent, and its supporting classes, including AbilityStartEventArgs and AbilityStartEventType. This addition enables handling and customization of ability start events in

---
+++ Assets/AbilitySystemExtension/Runtime/Events/AbilityStartEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using AbilitySystem.Runtime.Events;
+
+namespace AbilitySystemExtension.Runtime.Events
+{
+    public class AbilityStartEvent : GameplayEvent
+    {
+        public AbilityStartEvent(AbilityStartEventArgs arguments) : base(arguments)
+        {
+        }
+    }
+    
+    [Serializable]
+    public class AbilityStartEventArgs : EventArgs
+    {
+        public string AbilityName { get; private set;}
+        
+        public AbilityStartEventArgs(string abilityName)
+        {
+            AbilityName = abilityName;
+        }
+    }
+
+    [Serializable]
+    public class AbilityStartEventType : GameplayEventType
+    {
+        public override Type EventType => typeof(PlayerDeathEvent);
+    }
+}
