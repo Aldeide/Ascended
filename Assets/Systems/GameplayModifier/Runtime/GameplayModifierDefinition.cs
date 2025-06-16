20637b735ef796696f266986ce9d6daa06a0aef6
Aldeide <anthony.surleraux@gmail.com>
2025-06-16 22:39:22 +0200

    Add GameplayModifier system and localization assets
    Introduced a GameplayModifier system with definition and localization support. Added related assets, including Google Sheets service and localization addressable group rules, to integrate localization functionality within the system. These changes enable future extensibility and content localization.

---
+++ Assets/Systems/GameplayModifier/Runtime/GameplayModifierDefinition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Localization;
+
+namespace Systems.GameplayModifier.Runtime
+{
+    [Serializable]
+    [CreateAssetMenu(fileName = "GameplayModifier", menuName = "Ascended/GameplayModifier")]
+    public class GameplayModifierDefinition : ScriptableObject
+    {
+        public LocalizedString NameKey;
+        public LocalizedString DescriptionKey;
+    }
+}
