00f9febf4edfe19437fa8510b036e8d7d49b8b3c
Aldeide <anthony.surleraux@gmail.com>
2025-06-16 23:18:03 +0200

    Add GameplayModifierController and integrate with MainMenu
    Introduce a new `GameplayModifierController` script to manage gameplay modifiers, loading them dynamically at runtime. Updated the MainMenu scene to include the controller, ensuring proper integration for modifier functionality. Additionally, reorganized assets by moving `GameplayModifier.asset` into a `Resources` folder for runtime accessibility.

---
+++ Assets/Systems/GameplayModifier/Scripts/GameplayModifierController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Systems.GameplayModifier.Runtime;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Systems.GameplayModifier.Scripts
+{
+    public class GameplayModifierController : NetworkBehaviour
+    {
+        public List<GameplayModifierDefinition> AvailableModifiers = new();
+
+        private void Start()
+        {
+            var modifiers = Resources.LoadAll<GameplayModifierDefinition>("");
+            foreach (var modifier in modifiers)
+            {
+                availableModifiers.Add(modifier);
+            }
+        }
+    }
+}
