ea00a5e2d3b30b00e3c6aaa4a10b568176317c6a
Aldeide <anthony.surleraux@gmail.com>
2025-06-16 21:04:08 +0200

    Add new cue definitions for Visual Effects and Prefabs
    Introduced `VisualEffectCueDefinition` and `SpawnPrefabCueDefinition` classes to support visual effect and prefab spawning cues in the ability system. Updated `CueAudioDefinition` to standardize the menu name for consistency. This enhances the modularity and flexibility of the cue system.

---
+++ Assets/AbilitySystem/Runtime/Cues/VisualEffectCueDefinition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace AbilitySystem.Runtime.Cues
+{
+    [Serializable]
+    [CreateAssetMenu(fileName = "VisualEffectCue", menuName = "AbilitySystem/Cues/VisualEffectCue")]
+    public class VisualEffectCueDefinition : CueDefinition
+    {
+        public VisualEffectAsset VisualEffectAsset;
+    }
+}
