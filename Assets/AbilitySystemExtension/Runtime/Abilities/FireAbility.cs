b3a6bb61c96572940de5443c9771d2dbc875aedf
Aldeide <anthony.surleraux@gmail.com>
2025-06-17 21:49:35 +0200

    Refactor `FireAbility` impact cue playback and update scene settings
    Updated `FireAbility` to use hit point for impact visual effects instead of target position, ensuring proper effect placement. Adjusted scene lighting, volume profile settings, and layer configurations to enhance rendering accuracy and scene performance.

--- Assets/AbilitySystemExtension/Runtime/Abilities/FireAbility.cs
+++ Assets/AbilitySystemExtension/Runtime/Abilities/FireAbility.cs
@@ -18,18 +18,21 @@ namespace AbilitySystemExtension.Runtime.Abilities
             var muzzle = abilityData.MuzzlePosition;
             var impact = ((FireAbilityDefinition)Definition).impactVisualEffect;
             PlayActivationCues();
-            if (impact)
-            {
-                var data = new CueData
-                {
-                    VectorData = new[] {target, muzzle}
-                };
-                Owner.PlayCue(impact, data);
-            }
+
 
             var ray = new Ray(muzzle, target - muzzle);
             if (!Physics.Raycast(ray, out var hit, 100f, ((FireAbilityDefinition)Definition).layerMask)) return;
             Debug.DrawLine(muzzle, hit.point, Color.red, 1.0f);
+            
+            if (impact)
+            {
+                var data = new CueData
+                {
+                    VectorData = new[] {hit.point, muzzle}
+                };
+                Owner.PlayCue(impact, data);
+            }
+            
             var asc = hit.collider.GetComponent<AbilitySystemComponent>();
             if (!asc) return;
             asc.ExecuteEffect(((FireAbilityDefinition)Definition).damageEffect, Owner);
