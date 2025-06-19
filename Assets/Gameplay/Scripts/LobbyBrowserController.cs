d3e085327c9839908ada22b52bbea06eacee818d
Aldeide <anthony.surleraux@gmail.com>
2025-06-19 17:00:02 +0200

    Add `LobbyBrowserController` and update main menu for lobby management
    Introduced the `LobbyBrowserController` to handle lobby browsing functionality, including lobby list initialization, refresh, and navigation. Updated the main menu interface to support the new lobby flow, adding buttons for "Join Lobby" and "Refresh Lobbies." Refactored related UI and added reusable `AspectRatioElement` for responsive layout. Enabled Steam lobby creation via "Host" button.

---
+++ Assets/Gameplay/Scripts/LobbyBrowserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Interface;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Gameplay.Scripts
+{
+    public class LobbyBrowserController
+    {
+        private MainMenuController _mainMenuController;
+        private VisualElement _lobbyBrowserVisualElement;
+        
+        // Buttons
+        private Button _refreshButton;
+        private Button _mainMenuButton;
+        private Button _joinLobbyButton;
+        
+        // Lobby List
+        private ListView _lobbyListView;
+        private LobbyListController _lobbyListController;
+        
+        public void Initialise(VisualElement lobbyVisualElement, MainMenuController mainMenuController)
+        {
+            Debug.Log("Init LobbyBrowserController");
+            _mainMenuController = mainMenuController;
+            _lobbyBrowserVisualElement = lobbyVisualElement;
+            _refreshButton = _lobbyBrowserVisualElement.Q<Button>("RefreshLobbiesButton");
+            _mainMenuButton = _lobbyBrowserVisualElement.Q<Button>("ExitButton");
+            _joinLobbyButton = _lobbyBrowserVisualElement.Q<Button>("JoinLobbyButton");
+            
+            _lobbyListView = _lobbyBrowserVisualElement.Q<ListView>("LobbyListView");
+            _lobbyListController = new LobbyListController();
+            _lobbyListController.InitialiseList(_lobbyBrowserVisualElement, _lobbyListView.itemTemplate);
+            
+            _refreshButton.clicked += RefreshLobbies;
+            _mainMenuButton.clicked += _mainMenuController.ShowMainMenu;
+        }
+
+        private void RefreshLobbies()
+        {
+            Debug.Log("RefreshLobbies");
+            _lobbyListController.GetLobbies();
+        }
+    }
+}
