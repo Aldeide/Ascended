3157e990b92e9d44973497be3a7c4655f66ee43d
Aldeide <anthony.surleraux@gmail.com>
2025-07-09 23:40:05 +0200

    Add `TagQuery` struct and related runtime logic with tests for `GameplayTags`
    Introduced `TagQuery` and `TagCondition` structs to evaluate tag-based conditions in gameplay. Added support for various match types, including exact and partial matches. Implemented `TagsDropdown` for tag selection via Odin Inspector. Created associated unit tests to validate functionality and ensure correctness.

---
+++ Assets/Plugins/GameplayTags/Tests/Runtime/TagQueryTest.cs
@@ -0,0 +1,90 @@
+﻿using GameplayTags.Runtime;
+using NUnit.Framework;
+
+namespace Plugins.Tags.Tests.Runtime
+{
+    public class TagQueryTest
+    {
+          [Test]
+        public void TagQueryTest_AnyOfExactNonMatching_ReturnsFalse()
+        {
+            var testTagA = new Tag("Test.Tag.A");
+            var testTagB = new Tag("Test.Tag.B");
+            var testTagC = new Tag("Test.Tag.C");
+            var conditionOne = new TagCondition(TagMatchType.AnyOfExact, testTagA, testTagB);
+            var tagQuery = new TagQuery(conditionOne);
+            
+            Assert.IsFalse(tagQuery.MatchesTag(testTagC));
+        }
+        
+        [Test]
+        public void TagQueryTest_AnyOfExactMatching_ReturnsTrue()
+        {
+            var testTagA = new Tag("Test.Tag.A");
+            var testTagB = new Tag("Test.Tag.B");
+            var conditionOne = new TagCondition(TagMatchType.AnyOfExact, testTagA, testTagB);
+            var tagQuery = new TagQuery(conditionOne);
+            
+            Assert.IsTrue(tagQuery.MatchesTag(testTagB));
+        }
+        
+        [Test]
+        public void TagQueryTest_AnyOfPartialNonMatching_ReturnsFalse()
+        {
+            var testTagA = new Tag("Test.Tag.A");
+            var testTagB = new Tag("Test.Tag.B");
+            var testTagC = new Tag("Test.Tag.C");
+            var conditionOne = new TagCondition(TagMatchType.AnyOfPartial, testTagA, testTagB);
+            var tagQuery = new TagQuery(conditionOne);
+            
+            Assert.IsFalse(tagQuery.MatchesTag(testTagC));
+        }
+        
+        [Test]
+        public void TagQueryTest_AnyOfPartialMatching_ReturnsTrue()
+        {
+            var testTagA = new Tag("Test.Tag");
+            var testTagB = new Tag("Test.Tag.B");
+            var testTagC = new Tag("Test.Tag.C");
+            var conditionOne = new TagCondition(TagMatchType.AnyOfPartial, testTagA, testTagB);
+            var tagQuery = new TagQuery(conditionOne);
+            
+            Assert.IsTrue(tagQuery.MatchesTag(testTagC));
+        }
+        
+        [Test]
+        public void TagQueryTest_AllOfExactNonMatching_ReturnsFalse()
+        {
+            var testTagA = new Tag("Test.Tag.A");
+            var testTagB = new Tag("Test.Tag.B");
+            var testTagC = new Tag("Test.Tag.C");
+            var conditionOne = new TagCondition(TagMatchType.AllOfExact, testTagA, testTagB);
+            var tagQuery = new TagQuery(conditionOne);
+            
+            Assert.IsFalse(tagQuery.MatchesTags(new[] { testTagA, testTagC }));
+        }
+        
+        [Test]
+        public void TagQueryTest_AllOfExactMatching_ReturnsTrue()
+        {
+            var testTagA = new Tag("Test.Tag.A");
+            var testTagB = new Tag("Test.Tag.B");
+            var conditionOne = new TagCondition(TagMatchType.AllOfExact, testTagA, testTagB);
+            var tagQuery = new TagQuery(conditionOne);
+            
+            Assert.IsTrue(tagQuery.MatchesTags(new[] { testTagA, testTagB }));
+        }
+        
+        [Test]
+        public void TagQueryTest_AllOfPartialNonMatching_ReturnsFalse()
+        {
+            var testTagA = new Tag("Test.Tag.A");
+            var testTagB = new Tag("Test.Tag.B");
+            var testTagC = new Tag("Test.Tag.C");
+            var conditionOne = new TagCondition(TagMatchType.AllOfPartial, testTagA, testTagB);
+            var tagQuery = new TagQuery(conditionOne);
+            
+            Assert.IsFalse(tagQuery.MatchesTag(testTagC));
+        }
+    }
+}
