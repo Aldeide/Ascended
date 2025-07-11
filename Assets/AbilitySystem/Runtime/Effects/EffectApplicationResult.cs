f51591e1c9555867b7ffdc95e6b567e0bb1ef392
Aldeide <anthony.surleraux@gmail.com>
2025-07-11 18:19:10 +0200

    Moved some code to Plugins/. Moved external plugins to ThirdParty/

--- Assets/AbilitySystem/Runtime/Effects/EffectApplicationResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AbilitySystem.Runtime.Effects
-{
-    public enum EffectApplicationResult
-    {
-        Success = 1,
-        Immune = 2,
-        ApplicationRequiredTagsFailure = 3,
-    }
-}
