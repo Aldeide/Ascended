538b1353f17aac3a20cb98e469c5a605b39e95b2
Aldeide <anthony.surleraux@gmail.com>
2025-07-15 21:00:59 +0200

    Add `ActiveCueState` struct and enhance `AbilitySystemComponent` for networked cue synchronization
    Implemented `ActiveCueState` struct for managing active cue data with network serialization and equality checks. Updated `AbilitySystemComponent` to synchronize active cues with clients upon connection and added supporting methods for managing cue data in `CueManager`. Refactored `CueData` to struct and replaced `ActiveCues` list with a dictionary for efficient management.

---
+++ Assets/Plugins/AbilitySystem/Runtime/Cues/ActiveCueState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using GameplayTags.Runtime;
+using Unity.Netcode;
+
+namespace AbilitySystem.Runtime.Cues
+{
+    public struct ActiveCueState : INetworkSerializable, IEquatable<ActiveCueState>
+    {
+        public char[] CueTag;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref CueTag);
+        }
+
+        public bool Equals(ActiveCueState other)
+        {
+            return CueTag.Equals(other.CueTag);
+        }
+
+        public override int GetHashCode()
+        {
+            return CueTag.GetHashCode();
+        }
+    }
+
+}
