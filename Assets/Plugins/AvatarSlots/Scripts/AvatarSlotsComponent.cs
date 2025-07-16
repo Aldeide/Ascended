3e2b0b3763de23c2b3888a5703039098322b4693
Aldeide <anthony.surleraux@gmail.com>
2025-07-16 20:50:31 +0200

    Add `AvatarSlots` system for configurable avatar slot management
    Implemented the `AvatarSlotsComponent` and associated classes (`AvatarSlot`, `SlottableComponent`, `SlottableConfiguration`) to manage and configure avatar slots for slottable objects. Updated `GameplayTags` to include new slot-related tags. Integrated the system into the `Player` prefab while ensuring network support and editor enhancements.

---
+++ Assets/Plugins/AvatarSlots/Scripts/AvatarSlotsComponent.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GameplayTags.Generated;
+using GameplayTags.Runtime;
+using Plugins.AvatarSlots.Runtime;
+using Sirenix.OdinInspector;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace AvatarSlots.Scripts
+{
+    public class AvatarSlotsComponent : NetworkBehaviour
+    {
+        public List<AvatarSlot> AvatarSlotConfiguration = new();
+
+        private readonly Dictionary<Tag, Transform> _avatarSlots = new();
+        private readonly Dictionary<Tag, GameObject> _slottedObjects = new();
+
+        // Development only.
+        public GameObject SlotPrefab;
+
+        public override void OnNetworkSpawn()
+        {
+            base.OnNetworkSpawn();
+            Initialise();
+            if (IsServer)
+            {
+                NetworkManager.OnClientConnectedCallback += OnClientConnected;
+            }
+        }
+
+        private void Initialise()
+        {
+            foreach (var slot in AvatarSlotConfiguration)
+            {
+                _avatarSlots.Add(slot.SlotTag, slot.Transform);
+                _slottedObjects.Add(slot.SlotTag, null);
+            }
+            SlotGameObject(Instantiate(SlotPrefab), TagLibrary.SlotHipRight);
+        }
+
+        public void OnClientConnected(ulong clientId)
+        {
+            // Might not be required for NetworkedPrefabs?
+        }
+
+        public void SlotGameObject(GameObject go, Tag gameplayTag)
+        {
+            if (!go.TryGetComponent<SlottableComponent>(out var slottableComponent))
+            {
+                Debug.LogWarning($"The GameObject '{go.name}' is missing a SlottableComponent and cannot be slotted.");
+                return;
+            }
+
+            if (!_avatarSlots.ContainsKey(gameplayTag) || !_slottedObjects.ContainsKey(gameplayTag)) return;
+            go.transform.SetParent(_avatarSlots[gameplayTag]);
+            var config = slottableComponent.SlottableConfigurations.FirstOrDefault(c => c.Slot == gameplayTag);
+            if (config.Slot == gameplayTag)
+            {
+                go.transform.localPosition = config.PositionOffset;
+                go.transform.localRotation = config.RotationOffset;
+            }
+
+            _slottedObjects[gameplayTag] = go;
+        }
+
+        public void SwitchSlot(Tag source, Tag target)
+        {
+            if (_slottedObjects[source] == null || _slottedObjects[target] != null) return;
+            var go = _slottedObjects[source];
+            go.transform.SetParent(_avatarSlots[target]);
+
+            if (!go.TryGetComponent<SlottableComponent>(out var slottableComponent))
+            {
+                Debug.LogWarning($"The GameObject '{go.name}' is missing a SlottableComponent and cannot be slotted.");
+                return;
+            }
+
+            var config = slottableComponent.SlottableConfigurations.FirstOrDefault(c => c.Slot == target);
+            if (config.Slot == target)
+            {
+                go.transform.localPosition = config.PositionOffset;
+                go.transform.localRotation = config.RotationOffset;
+            }
+
+            _slottedObjects[target] = _slottedObjects[source];
+            _slottedObjects[source] = null;
+        }
+
+        public void RemoveSlottedGameObject(Tag gameplayTag)
+        {
+            if (!_slottedObjects.TryGetValue(gameplayTag, out var go)) return;
+            Destroy(go);
+            _slottedObjects[gameplayTag].transform.SetParent(null);
+            _slottedObjects[gameplayTag] = null;
+        }
+
+        public GameObject GetSlottedGameObject(Tag gameplayTag)
+        {
+            return _slottedObjects[gameplayTag];
+        }
+
+        [Button]
+        public void SwitchSides()
+        {
+            // Just to test the functionality.
+            if (_slottedObjects[TagLibrary.SlotHipRight])
+            {
+                SwitchSlot(TagLibrary.SlotHipRight, TagLibrary.SlotHipLeft);
+            }
+            else
+            {
+                SwitchSlot(TagLibrary.SlotHipLeft, TagLibrary.SlotHipRight);
+            }
+        }
+    }
+}
---
+++ Assets/Plugins/EternityCommon/Editor/OdinNetworkBehaviourEditor.cs
@@ -0,0 +1,9 @@
+﻿using Sirenix.OdinInspector.Editor;
+using Unity.Netcode;
+using UnityEditor;
+
+namespace EternityCommon.Editor
+{
+    [CustomEditor(typeof(NetworkBehaviour), true)]
+    public class OdinNetworkBehaviourEditor : OdinEditor {}
+}
