9590a9005292e1de240acef41891c84d01897423
Aldeide <anthony.surleraux@gmail.com>
2025-07-17 21:19:37 +0200

    Add comment to clarify the use of Odin Inspector in `OdinNetworkBehaviourEditor`

--- Assets/Plugins/EternityCommon/Editor/OdinNetworkBehaviourEditor.cs
+++ Assets/Plugins/EternityCommon/Editor/OdinNetworkBehaviourEditor.cs
@@ -4,6 +4,7 @@ using UnityEditor;
 
 namespace EternityCommon.Editor
 {
+    // Custom Editor class to use Odin Inspector for NetworkBehaviour.
     [CustomEditor(typeof(NetworkBehaviour), true)]
     public class OdinNetworkBehaviourEditor : OdinEditor {}
 }
