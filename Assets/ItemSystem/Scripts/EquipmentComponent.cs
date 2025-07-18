09987d75ca4e84e37236447e02dd3b0ca557f5f9
Aldeide <anthony.surleraux@gmail.com>
2025-07-18 14:36:53 +0200

    Add TODO comments for serialization in `EquipItem` and cleanup unused code in `EquipmentComponent`

--- Assets/ItemSystem/Scripts/EquipmentComponent.cs
+++ Assets/ItemSystem/Scripts/EquipmentComponent.cs
@@ -12,17 +12,17 @@ namespace ItemSystem.Scripts
     public class EquipmentComponent : NetworkBehaviour
     {
         public EquipmentManagerDefinition EquipmentManagerDefinition;
-        
+
         private EquipmentManager _equipmentManager;
         private AbilitySystemComponent _abilitySystemComponent;
-        
+
         public override void OnNetworkSpawn()
         {
             base.OnNetworkSpawn();
             // Only the client and the server should initialise this. Observers shouldn't have any information on
             // another player's equipment.
             if (!IsServer && !IsClient) return;
-            
+
             _abilitySystemComponent = GetComponent<AbilitySystemComponent>();
             // TODO: Figure out correct initialisation order (or maybe relying on an event is okay).
             if (_abilitySystemComponent.IsInitialized)
@@ -44,6 +44,8 @@ namespace ItemSystem.Scripts
         public void EquipItem(Tag slotName, EquipmentDefinition equipmentDefinition)
         {
             if (!IsClient) return;
+            // TODO: EquipmentDefinition probably isn't serialisable. Need to defined a serialised version that also
+            // contains mods to send over the network.
             _equipmentManager.Equip(slotName, equipmentDefinition);
         }
     }
