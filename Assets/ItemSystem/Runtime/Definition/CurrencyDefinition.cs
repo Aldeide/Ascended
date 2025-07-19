7973d8fbc1258a542c374e68b027bfdd8b90a057
Aldeide <anthony.surleraux@gmail.com>
2025-07-19 11:58:11 +0200

    Add `CurrencyDefinition` scriptable object, integrate `LocalizedString` for item names and descriptions, and update `ItemSystem` assembly definition. Fix typo in `EquipmentDefinition` comment.

---
+++ Assets/ItemSystem/Runtime/Definition/CurrencyDefinition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GameplayTags.Runtime;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace ItemSystem.Runtime.Definition
+{
+    [CreateAssetMenu(fileName = "CurrencyDefinition", menuName = "EquipmentSystem/CurrencyDefinition")]
+    public class CurrencyDefinition : ScriptableObject
+    {
+        [ValueDropdown("GetCurrencyDropdownValues", IsUniqueList = true, HideChildProperties = true)]
+        public Tag CurrencyTag;
+        
+        private IEnumerable<ValueDropdownItem> GetCurrencyDropdownValues()
+        {
+            return TagsDropdown.GameplayTagChoices.Where(tag => tag.Text.StartsWith("Currency."));
+        }
+    }
+}
--- Assets/ItemSystem/Runtime/Interface/IBaseItem.cs
+++ Assets/ItemSystem/Runtime/Interface/IBaseItem.cs
@@ -1,8 +1,10 @@
-﻿namespace ItemSystem.Runtime.Interface
+﻿using UnityEngine.Localization;
+
+namespace ItemSystem.Runtime.Interface
 {
     public interface IBaseItem
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public LocalizedString DisplayName { get; set; }
+        public LocalizedString Description { get; set; }
     }
 }
