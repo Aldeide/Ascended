8abc9edfc2ea321a29c278c04eeb7436aa381983
Aldeide <anthony.surleraux@gmail.com>
2025-08-20 17:58:28 +0200

    Add `UnitDealtDamageEvent` and related arguments to extend gameplay event system with damage tracking.

---
+++ Assets/SystemsExtensions/AbilitySystemExtension/Runtime/Events/UnitDealtDamageEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AbilitySystem.Runtime.Events;
+
+namespace AbilitySystemExtension.Runtime.Events
+{
+    [Serializable]
+    public class UnitDealtDamageEvent : GameplayEvent
+    {
+        public UnitDealtDamageEvent(UnitDealtDamageEventArgs arguments) : base(arguments)
+        {
+        }
+    }
+    
+    [Serializable]
+    public class UnitDealtDamageEventArgs : EventArgs
+    {
+        public float Amount { get; private set;}
+        
+        public UnitDealtDamageEventArgs(float amount)
+        {
+            Amount = amount;
+        }
+    }
+
+    [Serializable]
+    public class UnitDealtDamageEventType : GameplayEventType
+    {
+        public override Type EventType => typeof(UnitDealtDamageEvent);
+    }
+}
