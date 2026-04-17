4c9d286ff98bdbd4bede904cb82eaa2a92052207
Aldeide <anthony.surleraux@gmail.com>
2026-04-17 20:09:50 +0200

    Add `Vector2Node` to `AbilityGraph` for handling Vector2 input and output.

---
+++ Assets/Systems/AbilityGraph/Runtime/Nodes/Primitives/Vector2Node.cs
@@ -0,0 +1,20 @@
+﻿using AbilityGraph.Runtime.Nodes.Base;
+using System;
+using GraphProcessor;
+using UnityEngine;
+
+namespace AbilityGraph.Runtime.Nodes.Primitives
+{
+    [Serializable]
+    [NodeMenuItem("Primitives/Vector2")]
+    public class Vector2Node : AbilityNode {
+        [Output("Out")] public Vector2 Output;
+        [Input("In"), ShowAsDrawer] public Vector2 Input;
+
+        public override string name => "Vector2";
+
+        protected override void Process() {
+            Output = Input;
+        }
+    }
+}
