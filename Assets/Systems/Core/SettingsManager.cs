187810122e6e01c6a7af972a26d4abbcb1704b26
Aldeide <anthony.surleraux@gmail.com>
2026-04-17 23:51:26 +0200

    Add Unity test framework configuration and integrate `AttributeSet` editor tools with A.X.L. inspired design styles.

---
+++ Assets/Systems/Core/SettingsManager.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
+
+namespace Systems.Core
+{
+    public class SettingsManager : MonoBehaviour
+    {
+        public static SettingsManager Instance { get; private set; }
+
+        [Header("Input")]
+        [SerializeField] private InputActionAsset inputActions;
+
+        private const string QUALITY_KEY = "Settings_Quality";
+        private const string VSYNC_KEY = "Settings_VSync";
+        private const string VOLUME_KEY = "Settings_Volume";
+        private const string REBIND_KEY = "Settings_Rebinds";
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            LoadSettings();
+        }
+
+        public void SetQuality(int index)
+        {
+            QualitySettings.SetQualityLevel(index);
+            PlayerPrefs.SetInt(QUALITY_KEY, index);
+        }
+
+        public void SetVSync(bool enabled)
+        {
+            QualitySettings.vSyncCount = enabled ? 1 : 0;
+            PlayerPrefs.SetInt(VSYNC_KEY, enabled ? 1 : 0);
+        }
+
+        public void SetVolume(float volume)
+        {
+            AudioListener.volume = volume;
+            PlayerPrefs.SetFloat(VOLUME_KEY, volume);
+            // TODO: Hook into AudioMixer if available
+        }
+
+        public void SetResolution(int width, int height, bool fullScreen)
+        {
+            Screen.SetResolution(width, height, fullScreen);
+        }
+
+        public void SaveRebinds()
+        {
+            if (inputActions == null) return;
+            string rebinds = inputActions.SaveBindingOverridesAsJson();
+            PlayerPrefs.SetString(REBIND_KEY, rebinds);
+        }
+
+        private void LoadSettings()
+        {
+            // Quality
+            int quality = PlayerPrefs.GetInt(QUALITY_KEY, QualitySettings.GetQualityLevel());
+            QualitySettings.SetQualityLevel(quality, true);
+
+            // VSync
+            int vsync = PlayerPrefs.GetInt(VSYNC_KEY, QualitySettings.vSyncCount);
+            QualitySettings.vSyncCount = vsync;
+
+            // Volume
+            float volume = PlayerPrefs.GetFloat(VOLUME_KEY, AudioListener.volume);
+            AudioListener.volume = volume;
+
+            // Rebinds
+            if (inputActions != null && PlayerPrefs.HasKey(REBIND_KEY))
+            {
+                string rebinds = PlayerPrefs.GetString(REBIND_KEY);
+                inputActions.LoadBindingOverridesFromJson(rebinds);
+            }
+        }
+
+        public void ResetToDefaults()
+        {
+            PlayerPrefs.DeleteAll();
+            LoadSettings();
+            
+            // Clear input overrides
+            if (inputActions != null)
+            {
+                inputActions.RemoveAllBindingOverrides();
+            }
+        }
+    }
+}
