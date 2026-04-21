d471d219e93b1cb51e3f9911046641bbcb9838e5
Aldeide <anthony.surleraux@gmail.com>
2026-04-21 22:18:42 +0200

    Add unit tests for ability network and security policies
    Introduced tests to validate ability activation under various network and security policies including ClientOnly, Server, and ClientOrServer. Additionally, added `CreateTestAbilityDefinition` utility method to streamline the creation of test ability definitions for these scenarios.

---
+++ Assets/Systems/AbilitySystem/Test/Runtime/Abilities/AbilityPolicyTests.cs
@@ -0,0 +1,81 @@
+﻿using AbilitySystem.Runtime.Abilities;
+using GameplayTags.Runtime;
+using static AbilitySystem.Test.Utilities.AbilityUtilities;
+using static AbilitySystem.Test.Utilities.AbilitySystemUtilities;
+using NUnit.Framework;
+
+namespace AbilitySystem.Test.Runtime.Abilities
+{
+    public class AbilityPolicyTests
+    {
+        [Test]
+        public void AbilityPolicyTests_ClientOnly_ClientOrServer_ActivatesOnClient()
+        {
+            var mockClientAbilitySystem = CreateMockClientAbilitySystem();
+            var mockServerAbilitySystem = CreateMockServerAbilitySystem();
+            var clientAbilitySystem = mockClientAbilitySystem.Object;
+            var serverAbilitySystem = mockServerAbilitySystem.Object;
+            var abilityDefinition = CreateTestAbilityDefinition();
+            abilityDefinition.NetworkPolicy = AbilityNetworkPolicy.ClientOnly;
+            abilityDefinition.NetworkSecurityPolicy = AbilityNetworkSecurityPolicy.ClientOrServer;
+            clientAbilitySystem.AbilityManager.GrantAbility(abilityDefinition);
+            serverAbilitySystem.AbilityManager.GrantAbility(abilityDefinition);
+            
+            var eventDispatched = false;
+            clientAbilitySystem.AbilityManager.OnServerTryActivateAbilityRequested += (abilityName, key, data) =>
+            {
+                eventDispatched = true;
+            };
+            
+            Assert.IsFalse(eventDispatched, "Client tried to request server ability activation but shouldn't have.");
+            Assert.IsTrue(clientAbilitySystem.AbilityManager.TryActivateAbility(abilityDefinition.UniqueName));
+        }
+
+        [Test]
+        public void AbilityPolicyTests_ClientOnly_ClientOrServer_DoesNotActivateOnServer()
+        {
+            var mockClientAbilitySystem = CreateMockClientAbilitySystem();
+            var mockServerAbilitySystem = CreateMockServerAbilitySystem();
+            var clientAbilitySystem = mockClientAbilitySystem.Object;
+            var serverAbilitySystem = mockServerAbilitySystem.Object;
+            var abilityDefinition = CreateTestAbilityDefinition();
+            abilityDefinition.NetworkPolicy = AbilityNetworkPolicy.ClientOnly;
+            abilityDefinition.NetworkSecurityPolicy = AbilityNetworkSecurityPolicy.ClientOrServer;
+            clientAbilitySystem.AbilityManager.GrantAbility(abilityDefinition);
+            serverAbilitySystem.AbilityManager.GrantAbility(abilityDefinition);
+            
+            var eventDispatched = false;
+            clientAbilitySystem.AbilityManager.OnServerTryActivateAbilityRequested += (abilityName, key, data) =>
+            {
+                eventDispatched = true;
+            };
+            
+            Assert.IsFalse(eventDispatched, "Client tried to request server ability activation but shouldn't have.");
+            Assert.IsTrue(clientAbilitySystem.AbilityManager.TryActivateAbility(abilityDefinition.UniqueName));
+        }
+        
+        [Test]
+        public void AbilityPolicyTests_Server_ActivatesOnServer()
+        {
+            var mockServerAbilitySystem = CreateMockServerAbilitySystem();
+            var serverAbilitySystem = mockServerAbilitySystem.Object;
+            var abilityDefinition = CreateTestAbilityDefinition();
+            abilityDefinition.NetworkPolicy = AbilityNetworkPolicy.Server;
+            serverAbilitySystem.AbilityManager.GrantAbility(abilityDefinition);
+
+            Assert.IsTrue(serverAbilitySystem.AbilityManager.TryActivateAbility(abilityDefinition.UniqueName));
+        }
+        
+        [Test]
+        public void AbilityPolicyTests_Server_DoesNotActivatesOnClient()
+        {
+            var mockClientAbilitySystem = CreateMockClientAbilitySystem();
+            var clientAbilitySystem = mockClientAbilitySystem.Object;
+            var abilityDefinition = CreateTestAbilityDefinition();
+            abilityDefinition.NetworkPolicy = AbilityNetworkPolicy.Server;
+            clientAbilitySystem.AbilityManager.GrantAbility(abilityDefinition);
+
+            Assert.IsFalse(clientAbilitySystem.AbilityManager.TryActivateAbility(abilityDefinition.UniqueName));
+        }
+    }
+}
