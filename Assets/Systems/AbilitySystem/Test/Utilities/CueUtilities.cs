bfe41bc725194cc0175b7f6dde0b6ffd2bf62362
Aldeide <anthony.surleraux@gmail.com>
2026-04-28 21:08:56 +0200

    Refactor `CueManagerTests` to use `CueUtilities.CreateCueDefinitionWithTag`; add utility methods in `CueUtilities`

--- Assets/Systems/AbilitySystem/Test/Utilities/CueUtilities.cs
+++ Assets/Systems/AbilitySystem/Test/Utilities/CueUtilities.cs
@@ -1,7 +1,24 @@
-﻿namespace AbilitySystem.Test.Utilities
+﻿using AbilitySystem.Runtime.Cues;
+using GameplayTags.Runtime;
+using UnityEngine;
+
+namespace AbilitySystem.Test.Utilities
 {
     public static class CueUtilities
     {
+        public static CueDefinition CreateCueDefinitionWithTag(string cueTag)
+        {
+            var tag = new Tag(cueTag);
+            var cueDef = ScriptableObject.CreateInstance<CueDefinition>();
+            cueDef.CueTag = tag;
+            return cueDef;
+        }
         
+        public static CueDefinition CreateCueDefinitionWithTag(Tag cueTag)
+        {
+            var cueDef = ScriptableObject.CreateInstance<CueDefinition>();
+            cueDef.CueTag = cueTag;
+            return cueDef;
+        }
     }
 }
