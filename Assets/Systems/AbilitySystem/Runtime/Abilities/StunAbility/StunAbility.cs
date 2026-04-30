6327de0d44dcf6213634c7edfeb0f22a83d3e1ca
Aldeide <anthony.surleraux@gmail.com>
2026-04-30 22:23:30 +0200

    Refactor `StunAbility` effect application logic; add cancellation of active abilities with specific tags.

--- Assets/Systems/AbilitySystem/Runtime/Abilities/StunAbility/StunAbility.cs
+++ Assets/Systems/AbilitySystem/Runtime/Abilities/StunAbility/StunAbility.cs
@@ -1,5 +1,6 @@
 using AbilitySystem.Runtime.Abilities.Targeting;
 using AbilitySystem.Runtime.Core;
+using GameplayTags.Runtime;
 using UnityEngine;
 
 namespace AbilitySystem.Runtime.Abilities.StunAbility
@@ -18,26 +19,21 @@ namespace AbilitySystem.Runtime.Abilities.StunAbility
             var targetHandle = data.TargetData;
             foreach (var item in targetHandle.Data)
             {
-                if (item is TargetDataActor actorData)
+                if (item is not TargetDataActor actorData) continue;
+                // Resolve GameObject from NetworkObjectId
+                // For the sake of this implementation and tests, we'll use a simplified resolution
+                // In a full Unity project, this would use NetworkManager.Singleton.SpawnManager
+                var target = Owner.GetGameObjectFromNetworkId(actorData.NetworkObjectId);
+
+                if (!target) continue;
+                var targetAbilitySystem = target.GetComponent<IAbilitySystem>();
+                if (targetAbilitySystem == null) continue;
+                foreach (var grantedEffect in Definition.GrantedEffects)
                 {
-                    // Resolve GameObject from NetworkObjectId
-                    // For the sake of this implementation and tests, we'll use a simplified resolution
-                    // In a full Unity project, this would use NetworkManager.Singleton.SpawnManager
-                    GameObject target = Owner.GetGameObjectFromNetworkId(actorData.NetworkObjectId);
-                    
-                    if (target != null)
-                    {
-                        var targetAbilitySystem = target.GetComponent<IAbilitySystem>();
-                        if (targetAbilitySystem != null)
-                        {
-                            foreach (var grantedEffect in Definition.GrantedEffects)
-                            {
-                                var effect = MakeOutgoingEffect(grantedEffect);
-                                targetAbilitySystem.ApplyEffectToSelf(effect);
-                            }
-                        }
-                    }
+                    var effect = MakeOutgoingEffect(grantedEffect);
+                    targetAbilitySystem.ApplyEffectToSelf(effect);
                 }
+                targetAbilitySystem.AbilityManager.CancelAbilitiesWithTags(new[] {new Tag("Ability.Active")});
             }
             
             TryEndAbility();
