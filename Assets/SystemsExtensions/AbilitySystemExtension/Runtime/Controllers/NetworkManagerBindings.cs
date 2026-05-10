570ba74c4e277c442d3e95184693c02dac122c08
Aldeide <anthony.surleraux@gmail.com>
2026-05-10 16:58:23 +0200

    ✨ Add network spawn actions and bindings
    Add `SpawnHost`, `SpawnClient`, and `SpawnServer` input actions. Integrate `NetworkManagerBindings` to handle these actions and update Player prefab with relevant bindings and parameters.

---
+++ Assets/SystemsExtensions/AbilitySystemExtension/Runtime/Controllers/NetworkManagerBindings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace SystemsExtensions.AbilitySystemExtension.Runtime.Controllers
+{
+    public class NetworkManagerBindings : MonoBehaviour
+    {
+        [SerializeField] private NetworkManager _networkManager;
+        private void Awake()
+        {
+            this._networkManager = GetComponent<NetworkManager>();
+        }
+        
+        
+        public void OnSpawnHost(InputAction.CallbackContext context)
+        {
+            Debug.Log("OnSpawnHost");
+            if (context.phase == InputActionPhase.Performed)
+            {
+                Debug.Log("OnSpawnHostCalled");
+                _networkManager.StartHost();
+            }
+        }
+
+        public void OnSpawnClient(InputAction.CallbackContext context)
+        {
+            if (context.phase == InputActionPhase.Performed)
+            {
+                _networkManager.StartClient();
+            }
+        }
+
+        public void OnSpawnServer(InputAction.CallbackContext context)
+        {
+            if (context.phase == InputActionPhase.Performed)
+            {
+                _networkManager.StartServer();
+            }
+        }
+    }
+}
